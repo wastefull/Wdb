2cd645debf8dbf7982fa6cc86cd969e264877b18
Natto <thisis@natalies.email>
2026-06-16 14:03:47 -0700

    cleaning up styles and starting global.css

--- src/components/shared/LeftPanel.tsx
+++ src/components/shared/LeftPanel.tsx
@@ -55,21 +55,19 @@ function isUrl(str: string | undefined): boolean {
 }
 export function ResourcesPanel() {
   return (
-    <div className="p-4 flex flex-col items-center overflow-hidden ">
-      <h3 className="text-[13px] uppercase tracking-[0.08em] text-black/60 dark:text-white/60 mb-4 flex items-center gap-2">
-        Resources
-      </h3>
-      <aside className="px-1 py-3 bg-[#f5f4f1] dark:bg-[#2a2825] rounded-lg shrink-0">
-        <ul className="space-y-1">
+    <div className="resource-panel">
+      <h3>Resources</h3>
+      <aside>
+        <ul>
           {resources.map((r) => {
             const Icon = r.Icon;
             return (
               <li key={r.label}>
                 <a
                   href={isUrl(r.url) ? r.url : "#"}
-                  className={`flex items-center gap-2 px-2 py-1.5 rounded text-sm hover:bg-white cursor-pointer ${r.hidden ? "hidden" : ""}`}
+                  className={`${r.hidden ? "hidden" : ""}`}
                 >
-                  <Icon className="w-3.5 h-3.5 text-[#211f1c]/60 dark:text-white dark:hover:text-black/60" />
+                  <Icon />
                   {r.label}
                 </a>
               </li>
@@ -77,15 +75,12 @@ export function ResourcesPanel() {
           })}
         </ul>
 
-        <div
-          className={`arcade-card mt-8 p-3 rounded-lg border-[1.5px] border-[#211f1c] bg-waste-reuse text-center text-sm text-gray-800 dark:text-white/80 `}
-        >
+        <div className="card retro-card bg-waste-reuse!">
           <p className="text-sm uppercase tracking-[0.04em] leading-5">
             Can't find a material?
             <br />
             <br />
             <button
-              className={`card retro-card liquid text-sm inline-flex items-center gap-2 whitespace-nowrap px-3 py-2 m-auto border-border! border-[1.5px]! cursor-pointer`}
               onClick={() => {
                 document.getElementById("main-search-input")?.focus();
               }}
